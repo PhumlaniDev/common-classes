aee889db85dd9f358ff87f42bd5505107e3a9bde
Phumlani Arendse <aphumlani.dev@gmail.com>
2026-02-10 11:14:27 +0200

    feat: enhance TokenResponse with JSON property annotations

--- src/main/java/com/phumlanidev/commonevents/events/auth/TokenResponse.java
+++ src/main/java/com/phumlanidev/commonevents/events/auth/TokenResponse.java
@@ -1,5 +1,6 @@
 package com.phumlanidev.commonevents.events.auth;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +12,10 @@ import lombok.NoArgsConstructor;
 @Builder
 public class TokenResponse {
 
+    @JsonProperty("access_token")
     private String accessToken;
+    @JsonProperty("token_type")
     private String tokenType;
+    @JsonProperty("expires_in")
     private Long expiresIn;
 }
